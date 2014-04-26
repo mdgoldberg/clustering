14d70432a8fa40386610672b567791a52c9d2654
Nicholas Larus-Stone <nlarusstone@college.harvard.edu>
2014-04-25 21:16:52 -0400

    Kruskal

--- Kruskal.ml
+++ Kruskal.ml
@@ -1,34 +1,58 @@
-open Core
-open matrix.ml
-open signatures
+open Core.Std
+open Matrix
+open Signatures
 
-module Kruskal = struct
+module Kruskal = 
+struct
 
-	let loop (m : MATRIX.t) : PriorityQueue =
-		let q = PriorityQueue.make Comparable.compare in
-		let (a, b) = m.dimension in
+	let loop (m : M.t) : ('a Heap.t) =
+		let q = Heap.create ~cmp:compare () in
+		let (a, b) = M.dimensions m in
 		let size = max a b in
+		let _ =
 		for i = 0 to size do
 			for j = 0 to size do 
-				q.add (m.get (i, j), i, j)
-
-	let rec make_forest (m: MATRIX.t) (size : int): Set.t= 
-		if size = 0
-		then Set.empty
-		else Set.union (Set.singleton [size])
-
-	let is_spanning (trees: Set.t) (size: int) : bool =
-		let check = (List.fold_left ~f: (+) ~init: 0 tree) = (List.fold_left ~f: (+) ~init: 0 size) in
-		Set.iter check trees
-
-	let spanning (forest: Set.t) (edges: PriorityQueue) : 'a list = 
-		while not (PriorityQueue.empty edges) && not (is_spanning forest) do
-			let (e, v1, v2)= PriorityQueue.first edges in
-			PriorityQueue.remove_first edges;
-			
+				Heap.add q (M.get (i, j) m, i, j)
 			done
+		done 
+		in
+		q
 
+	(*let rec make_forest (m: M.t) (size : int): Int_set.t= 
+		if size = 0
+		then Int_set.empty
+		else Int_set.add(size) *)
 
-	let cluster (n: int) (m : MATRIX.t) : a' list list =
+	let is_spanning (trees: 'a Bag.t) (size: int) : bool =
+		let rec sizes (n : int) =
+			match n with
+			| 0 -> []
+			| x -> n :: (sizes x) in
+		(Bag.fold ~f: (+) ~init: 0 trees) = (List.fold_left ~f: (+) ~init: 0 (sizes size))
+		
+
+	
+
+	let remove_edges (tree: 'a list) (n: int) : 'a list list =
+		[[]]
+
+	let cluster (n: int) (m : ArrayMatrix(Matrix.FloatCompare).t) : 'a list list =
+		(*edges = loop m*)
+		(*size = m.dimension*)
+		(*forest = make_forest m size*)
+		(*tree = spanning forest edges*)
+		(*remove_edges tree n*)
+		let spanning (forest: 'a Bag.t) (edges: 'a Heap.t) : 'a list = 
+			while not (Heap.is_empty edges) && not (is_spanning forest n) 
+			do
+				(match Heap.top edges with
+				| None -> ()
+				| Some e -> ());
+				Heap.remove_top edges;
+				(*Check if the edge will connect two trees*)
+				(*Connect two trees*)
+			done;
+			[]
+		in
 		[[]]
 end
--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
-FILES=matrix.ml signatures.ml stats.ml markov.ml
+FILES=matrix.ml signatures.ml stats.ml markov.ml Kruskal.ml
 
 all: $(FILES)
 	corebuild matrix.native
 	corebuild signatures.native
 	corebuild stats.native
 	corebuild markov.native
+	corebuild Kruskal.native
